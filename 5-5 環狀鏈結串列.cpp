2a664721c5f6e453674a77b8bd79957abbd6894a
colalai <64138413+colalai@users.noreply.github.com>
2020-12-09 23:25:08 +0800

    Create 5-5 環狀鏈結串列.cpp

---
+++ 5-5 環狀鏈結串列.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+class ListNode
+{
+public:
+    ListNode(int a) { data = a; link = 0; }
+private:
+    int data;
+    ListNode* link;
+    friend class CircularList;
+};
+class CircularList
+{
+public:
+    CircularList();
+    void InsertFirst(int x);
+    void InsertLast(int x);
+    int DeleteFirst();
+    int DeleteLast();
+    void PrintList();
+private:
+    ListNode* last;
+};
+CircularList::CircularList()
+{
+    last = 0;
+}
+void CircularList::InsertFirst(int x)
+{
+    if (last == 0)
+    {
+        ListNode* newNode = new ListNode(x);
+        last = newNode;
+        last->link = last;
+    }
+    else {
+        ListNode* newNode = new ListNode(x);
+        newNode->link = last->link;
+        last->link = newNode;
+    }
+   
+
+}
+void CircularList::InsertLast(int x)
+{
+    if (last == 0)
+    {
+        ListNode* newNode = new ListNode(x);
+        last = newNode;
+        last->link = last;
+    }
+    else {
+        ListNode* newNode = new ListNode(x);
+        newNode->link = last->link;
+        last->link = newNode;
+        last = newNode;
+    }
+}
+int CircularList::DeleteFirst()
+{
+    
+    return 0;
+}
+int CircularList::DeleteLast()
+{
+    
+    return 0;
+}
+void CircularList::PrintList()
+{
+    ListNode* current = last->link;
+    
+    do{
+        cout << current->data << " ";
+        current = current->link;
+    } while (current != last->link);
+    cout << endl;
+}
+int main(int argc, char* argv[])
+{
+    CircularList list;
+    int times, input;
+    for (cin >> times; times > 0; times--) { cin >> input; list.InsertFirst(input); }
+    for (cin >> times; times > 0; times--) list.DeleteFirst();
+    for (cin >> times; times > 0; times--) { cin >> input; list.InsertLast(input); }
+    for (cin >> times; times > 0; times--) list.DeleteLast();
+    list.PrintList();
+}
