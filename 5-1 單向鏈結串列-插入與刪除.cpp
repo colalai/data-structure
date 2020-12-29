894deaf0498327ca46d6efc697e9eb4c67916564
colalai <64138413+colalai@users.noreply.github.com>
2020-12-29 12:02:57 +0800

    Update 5-1 單向鏈結串列-插入與刪除.cpp

--- 5-1 單向鏈結串列-插入與刪除.cpp
+++ 5-1 單向鏈結串列-插入與刪除.cpp
@@ -4,81 +4,80 @@ using namespace std;
 class ListNode
 {
 public:
-    ListNode(int a) { data = a; link = 0; }
+    ListNode(int a) {data = a; link = 0;}
 private:
     int data;
-    ListNode* link;
+    ListNode *link;
     friend class LinkedList;
 };
-
 class LinkedList
 {
 public:
-    LinkedList() :first(0) {};
     void InsertFirst(int x);
     void InsertLast(int x);
-    int DeleteFirst();
-    int DeleteLast();
+    void DeleteFirst();
+    void DeleteLast();
     void PrintList();
 private:
-    ListNode* first;
-    ListNode* last;
+    ListNode *first=0,*last=0;
 };
-
 void LinkedList::InsertFirst(int x)
 {
-    ListNode* newNode = new ListNode(x);
-    newNode->link = first;
-    first= newNode;
+    ListNode* newnode=new ListNode(x);
+    newnode->link=first;
+    first=newnode;
+    if(last==0)
+        last=newnode;
 }
 void LinkedList::InsertLast(int x)
 {
-    ListNode* newNode = new ListNode(x);
-    if (first == 0) {
-        first = newNode;
-        return;
+    ListNode* newnode=new ListNode(x);
+    if(last==0)
+    {
+        first=newnode;
+        last=newnode;
     }
-
-    ListNode* current = first;
-    while (current->link != 0) {
-        current = current->link;
+    else
+    {
+        last->link=newnode;
+        last=newnode;
     }
-    current->link = newNode;
 }
-int LinkedList::DeleteFirst()
+void LinkedList::DeleteFirst()
 {
-    ListNode* current = first;
-    first = current->link;
+    ListNode* current=first;
+    first=first->link;
     delete current;
-    current = 0; 
+    
 }
-int LinkedList::DeleteLast()
+void LinkedList::DeleteLast()
 {
-    ListNode* prev;
-    ListNode* current = first;
-    while(current->link!=0){
-        prev=current;
+    ListNode* current=first;
+    while(current->link!=last)
+    {
         current=current->link;
     }
-    delete current;
-    prev->link=0;
+    delete current->link;
+    current->link=0;
+    last=current;
+
 }
 void LinkedList::PrintList()
 {
-    ListNode* current = first;
-    while (current != 0) {
-        cout << current->data << " ";
-        current = current->link;
+    ListNode* current=first;
+    while(current!=0)
+    {
+        cout<<current->data<<" ";
+        current=current->link;
     }
-    cout << endl;
 }
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     LinkedList list;
     int times, input;
-    for (cin >> times; times > 0; times--) { cin >> input; list.InsertFirst(input); }
-    for (cin >> times; times > 0; times--) list.DeleteFirst();
-    for (cin >> times; times > 0; times--) { cin >> input; list.InsertLast(input); }
-    for (cin >> times; times > 0; times--) list.DeleteLast();
+    for(cin >> times; times > 0; times--) {cin >> input; list.InsertFirst(input);}
+    for(cin >> times; times > 0; times--) list.DeleteFirst();
+    for(cin >> times; times > 0; times--) {cin >> input; list.InsertLast(input);}
+    for(cin >> times; times > 0; times--) list.DeleteLast();
     list.PrintList();
 }
